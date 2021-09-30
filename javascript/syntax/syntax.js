c62b80bad328e51c72bfedbb928c97b3fc47ac4d
nethrarao <90671112+nethrarao@users.noreply.github.com>
2021-09-30 09:21:50 +0530

    updated by nethra
    	updated by nethra

---
+++ javascript/syntax/syntax.js
@@ -0,0 +1,19 @@
+function myFunction() {
+	document.getElementById("demo1").innerHTML= 11.23;
+	document.getElementById("demo2").innerHTML= "John Doe";
+	document.getElementById("demo3").innerHTML= (2+7)*10;
+	a=3;
+	b=5;
+	c=a*b;
+	document.getElementById("demo4").innerHTML= c;
+	A=5;
+	B=4;
+	C=A*B;
+	document.getElementById("demo5").innerHTML= C;
+	
+	
+	
+	
+	
+	
+	}
---
+++ javascript/whereto/whereto.js
@@ -0,0 +1,3 @@
+function externalFunction(){
+	document.getElementById("demo").innerHTML="This Text is comming from external js file";
+}
